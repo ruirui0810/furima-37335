944c349b16401ec256fd15d3a1bf5f7166a9bc9d
ruirui0810 <jobonly2525@gmail.com>
2022-04-22 16:56:18 +0900

    バリテーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :fast_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :fast_name_kana, presence: true
+         validates :birth_date, presence: true
+
 end
