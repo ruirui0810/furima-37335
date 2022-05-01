7033dba3926b4fe55190c6bc444b79d1d1728871
ruirui0810 <jobonly2525@gmail.com>
2022-05-01 19:42:03 +0900

    プルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_free_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
+
+
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_shipping_free_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :item_price, presence: true
+  validates :user, presence: true
 end
