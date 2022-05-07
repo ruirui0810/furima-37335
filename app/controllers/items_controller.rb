7e8114acadee299068eecd4ba094d5165b3fcc8c
ruirui0810 <jobonly2525@gmail.com>
2022-05-07 23:10:54 +0900

    レビュー後の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -51,5 +49,8 @@ private
     end
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
 end
