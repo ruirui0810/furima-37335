da025c6ddddb80611919e0ec829ebc99d9a83c14
ruirui0810 <jobonly2525@gmail.com>
2022-05-08 11:34:39 +0900

    レビュー後の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
 
   def index
@@ -43,7 +43,6 @@ private
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     if @item.user_id != current_user.id
       redirect_to action: :index
     end
