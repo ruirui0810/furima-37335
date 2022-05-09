76baceff07e6dccac67b75432f111c9a0ef46fc2
ruirui0810 <jobonly2525@gmail.com>
2022-05-09 22:20:55 +0900

    FactoryBotの設定

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-1234' }
+    item_prefecture_id{ '2' }
+    city { '横浜市' }
+    addresses { '青山1-1-1' }
+    building { '東京ハイツ' }
+    phone_number { '09012341234' }
+    association :order
+  end
+end
