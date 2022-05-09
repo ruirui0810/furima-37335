120e57b2762c8a22dda51f05fe68e05215e1ab7e
ruirui0810 <jobonly2525@gmail.com>
2022-05-09 20:16:18 +0900

    コントローラーのインスタンス生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_address = OrderAddress.new
   end
   
   def create
+    @order_address = OrderAddress.new(order_paramd)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to order_path
+    else
+      render :index
+    end
   end
 
   private
 
-  
+  def order_params
+    params.require(:order_address).permit(:postal_code,:item_prefecture_id, :city, :addresses, :building, :phone_number, :order).merge(user_id: current_user.id)
+  end
+
 end
